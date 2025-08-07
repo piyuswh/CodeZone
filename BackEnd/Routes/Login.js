eea0c9c26361fd764611abf39d2e3032b403aacb
Piyush <quantumplays660@gmail.com>
2025-08-07 23:39:04 +0530

    Update Login.js

--- BackEnd/Routes/Login.js
+++ BackEnd/Routes/Login.js
@@ -14,7 +14,11 @@ router.post('/logindata',async (req,res)=>{
 let status=await bcrypt.compare(req.body.password,user.password)
 if(status)
      {let token=jwt.sign({email:user.email},process.env.JWT_SECRET)
-            res.cookie("token",token)
+            res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'None'
+});
         return res.status(200).send("Login Succesfull")}
             else{
                 res.status(404).send("Something Went Wrong")
